f1b4727ef6be075ec6ef79b34166f1b74bbc62fa
contumax8 <mrtolgaydin249@gmail.com>
2020-07-21 20:22:12 +0300

    21.07.2020

--- Tolga/builtinmodules.py
+++ Tolga/builtinmodules.py
@@ -1,2 +1,9 @@
 import os 
-print(os.getcwd())
+liste1 = list()
+liste = os.listdir(os.getcwd())
+for i in liste :
+    if len(i) >5 :
+        continue
+    else:
+        liste1.append(i)
+print(liste1)
--- Tolga/ilk.py
+++ Tolga/ilk.py
@@ -1,7 +1,6 @@
 class Elemanlar:
+    zam = 0.05
     def __init__(self,ad,soyad,maas):
         self.ad = ad
         self.soyad = soyad
         self.maas = maas
-e1 = Elemanlar("tolga","aydın",3444)
-print(e1.maas)
