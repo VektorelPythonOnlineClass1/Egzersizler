6355e6cdb9960bcde4e7ee8cd0733582b40f125f
hunkargenchcttp <67922360+hunkargenchcttp@users.noreply.github.com>
2020-08-07 19:17:28 +0300

    07.08.2020 hünkar ilk ders

--- Hunkar/WebProjesi/VektorelWeb/urls.py
+++ Hunkar/WebProjesi/VektorelWeb/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+FROM django.views.generic import TemplateView
 
 urlpatterns = [
+    path('',TemplateView.as_view)
     path('admin/', admin.site.urls),
-    path('',include("Iletisim.urls")),
+    path('iletisim',include("Iletisim.urls")),
 ]
