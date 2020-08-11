674e35117e1b17a3326562295745ab7571883556
SinanToykun4160 <68555929+SinanToykun4160@users.noreply.github.com>
2020-08-11 16:29:54 +0300

    11.08.2020 16:30
    Tek siparişno ile birden fazla bilgi erkana aktarılması.

--- Sinan/proje3/proje3.py
+++ Sinan/proje3/proje3.py
@@ -2,19 +2,14 @@ from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import uic
 import sys
 
-
 import time
 import pandas as pd
 import numpy as np
 import itertools
 
-
-
 from openpyxl import Workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo 
 import xlrd
-
-
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -23,7 +18,6 @@ from selenium.webdriver.common.keys import Keys
 #not: Excel dosyalar SQL verileri çekebilir. Dolayısyla verileri değişebilir. 
 
 df = pd.read_excel("Sinan\proje3\TEST_EXCEL_short.xlsx")
-
 
 class App(QMainWindow):
     def __init__(self):
@@ -42,32 +36,36 @@ class App(QMainWindow):
 
 #kullanıcıdan gelecek olan 
 #ara butonun fonksiyonu
-
     def ara(self,caribilgi):
+        global i
+        i = 0
         global siparisno
-
+        
         #kullanıcıdan orderno giris yapılması
         siparisno = self.win.carikodugiris.text()
-
-        #beş parça kutucuktan oluşan ikinci kutucuk ilk etapta Customername,Customer,Salesrep,Productcode ve Productname göstermektedir. 
+        print(len(df))
+        
+        #beş parça kutucuktan oluşan ikinci kutucuk ilk etapta Customername,Customer,Salesrep,Productcode ve Productname göstermektedir.
+       
+        #if int(siparisno) == self.df.ORDNERNO.loc[i] == False:
+        
+        #else:
         #Excel deki C Kolon
-        caribilgi = self.win.caribilgi.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[0].Customername))
-        #Excel deki B Kolon
-        caribilgi_2 = self.win.caribilgi_2.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[0].Customer_no))
-        #Excel deki D Kolon
-        caribilgi_4 = self.win.caribilgi_4.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[0].salesrep))
-        #Excel deki E Kolon
-        caribilgi_3 = self.win.caribilgi_3.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[0].productcode))
-        #Excel deki F Kolon
-        caribilgi_5 = self.win.caribilgi_5.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[0].productname))
-        #Excel deki F Kolon
-        caribilgi_6 = self.win.caribilgi_6.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[0].Amount))
-        #Excel deki F Kolon
-        caribilgi_7 = self.win.caribilgi_7.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[0].Price))
-        if not True:
-
-            print("Böyle bir siparişno yok")      
-
+        caribilgi = self.win.caribilgi.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[i].siparisno))
+    #Excel deki B Kolon
+        caribilgi_2 = self.win.caribilgi_2.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[i].Customer_no))
+    #Excel deki D Kolon
+        caribilgi_4 = self.win.caribilgi_4.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[i].salesrep))
+    #Excel deki E Kolon
+        caribilgi_3 = self.win.caribilgi_3.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[i].productcode))
+    #Excel deki F Kolon
+        caribilgi_5 = self.win.caribilgi_5.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[i].productname))
+    #Excel deki F Kolon
+        caribilgi_6 = self.win.caribilgi_6.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[i].Amount))
+    #Excel deki F Kolon
+        caribilgi_7 = self.win.caribilgi_7.setText(str(self.df[self.df.ORDNERNO==int(siparisno)].loc[i].Price))
+        if caribilgi == False:
+            i = i + 1
 #temizle butonun fonksiyonu
 
     def temizle(self):
@@ -84,8 +82,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
-
-
 
     #def otomatik(self):
     #    self.win.txtUserInput.setText(#"buraya kullanıcıadınızı giriniz")
