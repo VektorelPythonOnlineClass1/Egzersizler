c4f7e4980f693a8f2551c2f558271b6d1a1865ce
hunkargenchcttp <67922360+hunkargenchcttp@users.noreply.github.com>
2020-11-15 18:23:56 +0300

    x

--- Hunkar/WebProjesi/Iletisim/views.py
+++ Hunkar/WebProjesi/Iletisim/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from  .models import iletisimModel
 
 def iletisimListele(request):
+    mesajlar = iletisimModel.objects.all()
     return render(request,'iletisim/iletisimListe.html',{})
